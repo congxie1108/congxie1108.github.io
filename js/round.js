dcf90819f30e1d51ffbf3d8646e997a88893a127
Cong Xie <cong.xie@bytedance.com>
2024-12-30 15:27:08 -0800

    first commit

---
+++ js/round.js
@@ -0,0 +1,5 @@
+﻿$.fn.roundblock = function(nopadding) {    
+  $(this).css("border-radius", $(this).css("height"));   
+  if(nopadding != true)
+	$(this).css("padding", Math.ceil($(this).height()/5.65685)+5 + "px");
+};    
